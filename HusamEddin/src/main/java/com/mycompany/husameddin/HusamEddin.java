4ce681a5d7c9780c9a330501ee0e1f859099ba88
fireburn2k14 <h.musbah@wahaoil.ly>
2022-08-10 13:05:57 +0200

    first commit

---
+++ HusamEddin/src/main/java/com/mycompany/husameddin/HusamEddin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.husameddin;
+
+/**
+ *
+ * @author almadkhal7
+ */
+public class HusamEddin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
